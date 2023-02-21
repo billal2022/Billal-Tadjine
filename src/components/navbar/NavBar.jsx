e2c69b00b4f5d3a8bd070e1d01a22a58e2e03560
billal2022 <billal.tj@outlook.com>
2023-02-21 09:57:28 +0300

    update

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -6,23 +6,23 @@ import './Navbar.css';
 
 const BasicExample = () => {
   const [isMenuClicked, setIsMenuClicked] = useState(false)
-  const [menu_class, setMenuClass] = useState("app__navbar hidden")
+  const [menu_class, setMenuClass] = useState("app__navbar-links hidden")
   const [open, setopen] = useState("openIcon open")
   
   const updateMenu = () => {
     if(!isMenuClicked) {
-        setMenuClass("app__navbar visible")
+        setMenuClass("app__navbar-links visible")
         setopen("openIcon close")
     
     }
     else {
-        setMenuClass("app__navbar hidden")
+        setMenuClass("app__navbar-links hidden")
         setopen("openIcon open")
     }
     setIsMenuClicked(!isMenuClicked)
   }
   return (
-    <nav className={menu_class} >
+    <nav style={{backgroundColor:window.scrollY >= 100?"red":""}} className="app__navbar">
       <div className="app__navbar-logo">  
      <img src={img}/>
      <h1 className={"app__navbar-title"}>BILLAL TADJINE</h1>
@@ -32,7 +32,7 @@ const BasicExample = () => {
           <span class="icon-bar middle-bar"></span>
           <span class="icon-bar bottom-bar"></span>
         </button>
-      <ul className="app__navbar-links">
+      <ul className={menu_class}>
         <li className="p__opensans"><a href="#Home">Home</a></li>
         <li className="p__opensans"><a href="#About">About</a></li>
         <li className="p__opensans"><a href="#Projects">Projects</a></li>
