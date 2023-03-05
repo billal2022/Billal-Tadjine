a8b463197894e67faf036d3edad01b5340aaeba9
billal2022 <billal.tj@outlook.com>
2023-03-05 18:06:35 +0300

    update 3.5

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -5,11 +5,16 @@ import './Navbar.css';
 const BasicExample = () => {
   const [isMenuClicked, setIsMenuClicked] = useState(false)
   const [menu_class, setMenuClass] = useState("links hidden")
-  const [open, setopen] = useState("openIcon open")
+  const [checked, setopen] = useState(false)
   const [scrollEffect, setscrollEffect] = useState(false)
-
+//humburgar toggle
+  const HandleChange = () => {
+    setopen(!checked);
+  };
+  //Navbar change color
   useEffect(() => {
 const Changebg=()=>{  
+
     const COLOR_CHANGE_THRESHOLD = 90;
 
   if(window.scrollY >= COLOR_CHANGE_THRESHOLD && scrollEffect !== true){
@@ -21,35 +26,39 @@ const Changebg=()=>{
        
        window.addEventListener("scroll",Changebg )
 }, )
-  
+  //toggling function
   const updateMenu = () => {
     if(!isMenuClicked) {
         setMenuClass("links visible")
-        setopen("openIcon close")
+   setopen(checked)
     
     }
     else {
         setMenuClass("links hidden")
-        setopen("openIcon open")
+        setopen(!checked)
     }
     setIsMenuClicked(!isMenuClicked)
   }
   return (
     <nav  className={scrollEffect?"app__navbar scroll" : "app__navbar"}>
       <div className="app__navbar-logo">  
-     <span  class="icon-atom logo"></span>
+     <span  className="icon-atom logo"></span>
      <h1 className={"app__navbar-title"}>BILLAL TADJINE</h1>
       </div>
-      <button onClick={updateMenu} className={open} >
-          <span className="icon-bar top-bar"></span>
-          <span className="icon-bar middle-bar"></span>
-          <span className="icon-bar bottom-bar"></span>
-        </button>
+     
+      <label className="hamburger" >
+  <input type="checkbox" checked={checked} onChange={HandleChange} onClick={updateMenu}/>
+  <svg viewBox="0 0 32 32">
+    <path className="line line-top-bottom" d="M27 10 13 10C10.8 10 9 8.2 9 6 9 3.5 10.8 2 13 2 15.2 2 17 3.8 17 6L17 26C17 28.2 18.8 30 21 30 23.2 30 25 28.2 25 26 25 23.8 23.2 22 21 22L7 22"></path>
+    <path className="line" d="M7 16 27 16"></path>
+  </svg>
+</label>
+        
       <ul className={menu_class}>
         <li  className="p__opensans"><Link activeClass="active"  spy={true} onClick={updateMenu} to="Home" >Home</Link></li>
-        <li className="p__opensans"><Link spy={true} onClick={updateMenu} to="About">About</Link></li>
-        <li className="p__opensans"><Link  spy={true}onClick={updateMenu} to="Projects">Projects</Link></li>
-        <li className="p__opensans"><Link spy={true} onClick={updateMenu} to="Contact">Contact</Link></li>
+        <li className="p__opensans"><Link offset={-30} spy={true} onClick={updateMenu} to="About">About</Link></li>
+        <li className="p__opensans"><Link offset={-30}  spy={true}onClick={updateMenu} to="Projects">Projects</Link></li>
+        <li className="p__opensans"><Link offset={-30} spy={true} onClick={updateMenu} to="Contact">Contact</Link></li>
       </ul>
     </nav>
   );
